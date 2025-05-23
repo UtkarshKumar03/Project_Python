7a1188376f38996e0cb44daee6b961864898fae8
Utkarsh Kumar <utkarsh.srf03@gmail.com>
2025-05-23 17:55:12 +0530

    task-2

---
+++ Assignment_3/calculate_Math_module.py
@@ -0,0 +1,9 @@
+import math
+num = int(input("Enter the number: "))
+square_root = math.sqrt(num)
+log = math.log(num)
+sine = math.sin(num)
+
+print("Square root: ",square_root)
+print("Logarithm:",log)
+print("Sine:",sine)
